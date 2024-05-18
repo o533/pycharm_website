46f8fb90ce1852bbb62129e90575a92bf5d53290
22-101241 <o.ezz@bspt.live>
2024-05-18 14:28:23 +0200

    application Add readme and requirements

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
 alert('hi')
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Check if username and password match
+    if (username === 'عمر' && password === '123') {
+        alert('تم تسجيل الدخول بنجاح!');
+        // يمكنك هنا توجيه المستخدم إلى صفحة أخرى بعد تسجيل الدخول
+    } else {
+        alert('اسم المستخدم أو كلمة المرور غير صحيحة. الرجاء المحاولة مرة أخرى.');
+    }
+});
